a2485ece18044b90d307e115d4d0230889210931
ionce007 Huang <ionce@163.com>
2023-09-28 19:18:27 +0000

    改用mssql读取数据库

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 //const url = "http://www.foryet.net/api/actions.aspx?action="
 
+var sql = require('mssql'); 
+
+var config = {
+    user: 'qds169535493',  
+    password: 'Hwang113@doinnx', 
+    server: 'qds169535493.my3w.com',
+    database: 'qds169535493_db',
+    port: 1433,
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+};
+
 function get_file_path(){
-    
+    var conn = new sql.ConnectionPool(config);
+    conn.connect(function(err){
+        if(err) console.log(err); 
+        else  {
+            console.log('connect success!'); 
+            var req = new sql.Request(conn);
+            req.query('select * from select * from bdFile',function(err,res){ 
+                if(err) console.log(err); 
+                else console.log(res); 
+            })
+        }
+    });
 }
 
 function recieve_wx_auth_login(code){

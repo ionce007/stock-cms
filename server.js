beaae216b7394898036fa44f2f21a1192bfc48ec
ionce007 Huang <ionce@163.com>
2023-09-28 17:55:50 +0000

    修改server.js中的错误。

--- server.js
+++ server.js
@@ -15,17 +15,9 @@ function get_file_path(){
         debugger;
         console.log('statusCode - ', res.statusCode);
         console.log('contentLength - ', res.headers['content-length']);
-      
         if(res.statusCode == 200){
-          /*savedFd = fs.openSync('test.jpg', 'w');
-          if(savedFd > -1){
-            savedStream = fs.createWriteStream('test.jpg', {fd: savedFd});
-            console.log('write stream created.');
-            savedStream.on('finish', function(){
-              console.log('data saved finished.');
-              process.exit(0);
-            });
-          }*/
+            debugger;
+            console.log(res)
         }else{
             console.log('error occured.');
             process.exit(1);
@@ -37,30 +29,14 @@ function get_file_path(){
       
         res.on('close', function(){
             console.log('res closed, end save now.');
-            //endSaved();
         });
         res.on('end', function(){
             console.log('res end, end save now.');
-          //endSaved();
         });
-        res.on('error',  function(e){
+        res.on('error', function(e){
             console.log('res err, end save now.');
-          //endSaved();
         });
-    /*request(url + 'get_file_path', function (error, response, body) {   //body为返回的数据
-        console.log('get_file_path');
-        debugger;
-        if (!error) {
-            console.log('success');
-            console.log(body); // 请求成功的处理逻辑
-            console.log(response);
-        } 
-        else{
-            console.log('error');
-            console.log(error);
-            console.log(response);
-        }
-    });*/
+    })
 }
 
 function recieve_wx_auth_login(code){
@@ -81,6 +57,7 @@ function fore_file_list(path){
         }
     });
 }
+
 module.exports.get_file_path = get_file_path();
 //module.exports.recieve_wx_auth_login = recieve_wx_auth_login(code);
 //module.exports.fore_file_list = fore_file_list(path);

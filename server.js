d4f73513a39ce1fb16ab6d3a425ef03929b963a1
ionce007 Huang <ionce@163.com>
2023-09-28 17:39:33 +0000

    使用http替代request测试

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 const url = "http://www.foryet.net/api/actions.aspx?action="
 var request = require('request');
 
+var http = require('http');
+var fs = require('fs');
+
 function get_file_path(){
     debugger;
-    request(url + 'get_file_path', function (error, response, body) {   //body为返回的数据
+    var req = http.get(url + 'get_file_path');
+
+    req.on('error', function(e){
+        console.log(e);
+    });
+    req.on('response', function(res){
+        debugger;
+        console.log('statusCode - ', res.statusCode);
+        console.log('contentLength - ', res.headers['content-length']);
+      
+        if(res.statusCode == 200){
+          /*savedFd = fs.openSync('test.jpg', 'w');
+          if(savedFd > -1){
+            savedStream = fs.createWriteStream('test.jpg', {fd: savedFd});
+            console.log('write stream created.');
+            savedStream.on('finish', function(){
+              console.log('data saved finished.');
+              process.exit(0);
+            });
+          }*/
+        }else{
+            console.log('error occured.');
+            process.exit(1);
+        }
+      
+        res.on('data', function(chunk){
+            console.log('got data - ', chunk);
+        });
+      
+        res.on('close', function(){
+            console.log('res closed, end save now.');
+            //endSaved();
+        });
+        res.on('end', function(){
+            console.log('res end, end save now.');
+          //endSaved();
+        });
+        res.on('error',  function(e){
+            console.log('res err, end save now.');
+          //endSaved();
+        });
+    /*request(url + 'get_file_path', function (error, response, body) {   //body为返回的数据
         console.log('get_file_path');
         debugger;
         if (!error) {
@@ -16,7 +60,7 @@ function get_file_path(){
             console.log(error);
             console.log(response);
         }
-    });
+    });*/
 }
 
 function recieve_wx_auth_login(code){

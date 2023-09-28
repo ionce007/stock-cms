0ec3da5cd187c25be9950333e9e6924f65252133
ionce007 Huang <ionce@163.com>
2023-09-28 16:53:48 +0000

    修改08.

--- bundle.js
+++ bundle.js
@@ -90732,6 +90732,6 @@ function fore_file_list(path){
     });
 }
 module.exports.get_file_path = get_file_path();
-module.exports.recieve_wx_auth_login = recieve_wx_auth_login(code);
-module.exports.fore_file_list = fore_file_list(path);
+//module.exports.recieve_wx_auth_login = recieve_wx_auth_login(code);
+//module.exports.fore_file_list = fore_file_list(path);
 },{"request":381}]},{},[268]);

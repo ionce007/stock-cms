7c64d1e74c0d88df1ddd4a120c76d34190eb1a51
ionce007 <ionce@163.com>
2023-09-29 15:08:25 +0000

    将mssql访问数据库改为mysql访问。

--- server.js
+++ server.js
@@ -1,5 +1,47 @@
 //const url = "http://www.foryet.net/api/actions.aspx?action="
 
+//node连接MySQL数据库
+const mysql = require ('mysql')
+//创建连接
+let conn=mysql.createConnection({
+    //主机地址
+    host:'mysql.sqlpub.com',
+    //端口号
+    port:3306,
+    //用户名
+    user:'ioncehm',
+    //密码
+    password:'0b1a6c1aa061f05b',
+    //数据库名称
+    database:'stockcms'
+})
+//获取连接
+conn.connect((err)=>{
+    debugger;
+    if(err) throw err;
+    console.log('连接成功');
+})
+//result->查询结果
+let sql1='select * from article';
+conn.query(sql1,(err,result)=>{
+    debugger;
+    if(err){
+        console.log(err.message);
+    }
+    else{
+        console.log('------------------');  
+        console.log(result);
+        console.log("查询完成");
+        console.log('------------------');  
+    }
+})
+//关闭数据库连接
+conn.end((err)=>{
+    if (err) throw err;
+    console.log("关闭成功");
+})
+
+/*
 var sql = require('mssql'); 
 //var mysql = require('mysql');
 var config = {
@@ -14,7 +56,59 @@ var config = {
         idleTimeoutMillis: 30000
     }
 };
+*/
+/*var Connection = require('tedious').Connection
+var Request = require('tedious').Request
 
+var config = {
+    userName: 'qds169535493', // update me
+    password: 'Hwang113@doinnx', // update me
+    server: 'qds169535493.my3w.com',
+    database: 'qds169535493_db',
+    port: 1433,
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+}
+
+var connection = new Connection(config)
+
+connection.on('connect', function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('DB connected success!')
+    }
+})
+
+function get_file_path () {
+    request = new Request("select * from select * from bdFile", function (err, rowCount) {
+        debugger;
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(rowCount + ' rows')
+        }
+        connection.close()
+    })
+  
+    request.on('row', function (columns) {
+        debugger;
+        columns.forEach(function (column) {
+            debugger;
+            if (column.value === null) {
+                console.log('NULL')
+            } else {
+                console.log(column.value)
+            }
+        })
+    })
+    connection.execSql(request)
+}
+*/
+/*
 function get_file_path(){
     debugger;
     var conn = new sql.ConnectionPool(config);
@@ -32,7 +126,7 @@ function get_file_path(){
         }
     });
 }
-
+*/
 function recieve_wx_auth_login(code){
     
 }
